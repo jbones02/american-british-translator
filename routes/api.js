f5c6bfc445964b968349efef90c0efa761ca5329
jbones02 <jaryd02@gmail.com>
2025-02-13 11:01:02 -0800

    Initial commit

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,49 @@ module.exports = function (app) {
   
   const translator = new Translator();
 
+  const translateBritishToAmerican = britishText => {
+
+  }
+
+  const translateAmericanToBritish = AmericanText => {
+    
+  }
+
+  const translate = (untraslatedText, originalLocale) => {
+    let returnObj;
+    if (!untranslatedText || !originalLocale) {
+      returnObj = { error: 'Required field(s) missing' }
+    } else if (originalLocale === 'american-to-british') {
+      returnObj = {
+        text: untraslatedText,
+        translation: translateBritishToAmerican(untraslatedText)
+      };
+    } else if (originalLocale === 'british-to-american') {
+      returnObj ={
+        text: untraslatedText,
+        translation: translateAmericanToBritish(untraslatedText);
+      };
+    } else {
+      returnObj = { error: 'Invalid value for locale field'}
+    }
+    return returnObj;
+  }
+
   app.route('/api/translate')
     .post((req, res) => {
-      
+      try {
+        const untraslatedText = req.body.text;
+        const originalLocale = req.body.locale;
+
+        if (!untraslatedText || !originalLocale) {
+          return res.status(200).json({ error: 'Required field(s) missing' });
+        } else {
+          return translate(untraslatedText, originalLocale);
+        }
+
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' })
+      }
     });
 };
